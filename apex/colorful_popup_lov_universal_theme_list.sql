e8bff71316444344983b958daaf67244632a21dc
Julian Stark <j.stark@buedingen-med.de>
2022-11-08 13:51:48 +0100

    Version 0.2

---
+++ apex/colorful_popup_lov_universal_theme_list.sql
@@ -0,0 +1,316 @@
+prompt --application/shared_components/user_interface/lovs/colorful_popup_lov_universal_theme_list
+begin
+--   Manifest
+--     COLORFUL_POPUP_LOV__UNIVERSAL_THEME_LIST
+--   Manifest End
+wwv_flow_api.component_begin (
+ p_version_yyyy_mm_dd=>'2021.10.15'
+,p_release=>'21.2.0'
+,p_default_workspace_id=>27635782693646703
+,p_default_application_id=>501
+,p_default_id_offset=>65309767924897831
+,p_default_owner=>'PROD'
+);
+wwv_flow_api.create_list_of_values(
+ p_id=>wwv_flow_api.id(65328375199630680)
+,p_lov_name=>'COLORFUL_POPUP_LOV__UNIVERSAL_THEME_LIST'
+,p_lov_query=>'.'||wwv_flow_api.id(65328375199630680)||'.'
+,p_location=>'STATIC'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65346611590580800)
+,p_lov_disp_sequence=>1
+,p_lov_disp_value=>'Info'
+,p_lov_return_value=>'u-info'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65346983682580800)
+,p_lov_disp_sequence=>2
+,p_lov_disp_value=>'Warning'
+,p_lov_return_value=>'u-warning'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65347365833580800)
+,p_lov_disp_sequence=>3
+,p_lov_disp_value=>'Danger'
+,p_lov_return_value=>'u-danger'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65347790244580800)
+,p_lov_disp_sequence=>4
+,p_lov_disp_value=>'Success'
+,p_lov_return_value=>'u-success'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65328676812630679)
+,p_lov_disp_sequence=>5
+,p_lov_disp_value=>'Color 1'
+,p_lov_return_value=>'u-color-1'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65329119755585726)
+,p_lov_disp_sequence=>6
+,p_lov_disp_value=>'Color 2'
+,p_lov_return_value=>'u-color-2'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65329460503585726)
+,p_lov_disp_sequence=>7
+,p_lov_disp_value=>'Color 3'
+,p_lov_return_value=>'u-color-3'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65329866014585725)
+,p_lov_disp_sequence=>8
+,p_lov_disp_value=>'Color 4'
+,p_lov_return_value=>'u-color-4'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65330221369585725)
+,p_lov_disp_sequence=>9
+,p_lov_disp_value=>'Color 5'
+,p_lov_return_value=>'u-color-5'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65330684951585725)
+,p_lov_disp_sequence=>10
+,p_lov_disp_value=>'Color 6'
+,p_lov_return_value=>'u-color-6'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65331067277585725)
+,p_lov_disp_sequence=>11
+,p_lov_disp_value=>'Color 7'
+,p_lov_return_value=>'u-color-7'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65331417748585724)
+,p_lov_disp_sequence=>12
+,p_lov_disp_value=>'Color 8'
+,p_lov_return_value=>'u-color-8'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65331822451585724)
+,p_lov_disp_sequence=>13
+,p_lov_disp_value=>'Color 9'
+,p_lov_return_value=>'u-color-9'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65332265557585724)
+,p_lov_disp_sequence=>14
+,p_lov_disp_value=>'Color 10'
+,p_lov_return_value=>'u-color-10'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65332617366585724)
+,p_lov_disp_sequence=>15
+,p_lov_disp_value=>'Color 11'
+,p_lov_return_value=>'u-color-11'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65333072764585724)
+,p_lov_disp_sequence=>16
+,p_lov_disp_value=>'Color 12'
+,p_lov_return_value=>'u-color-12'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65333489099585723)
+,p_lov_disp_sequence=>17
+,p_lov_disp_value=>'Color 13'
+,p_lov_return_value=>'u-color-13'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65333893633585723)
+,p_lov_disp_sequence=>18
+,p_lov_disp_value=>'Color 14'
+,p_lov_return_value=>'u-color-14'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65334250310585723)
+,p_lov_disp_sequence=>19
+,p_lov_disp_value=>'Color 15'
+,p_lov_return_value=>'u-color-15'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65334697393585723)
+,p_lov_disp_sequence=>20
+,p_lov_disp_value=>'Color 16'
+,p_lov_return_value=>'u-color-16'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65335026092585722)
+,p_lov_disp_sequence=>21
+,p_lov_disp_value=>'Color 17'
+,p_lov_return_value=>'u-color-17'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65335452816585722)
+,p_lov_disp_sequence=>22
+,p_lov_disp_value=>'Color 18'
+,p_lov_return_value=>'u-color-18'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65335838090585722)
+,p_lov_disp_sequence=>23
+,p_lov_disp_value=>'Color 19'
+,p_lov_return_value=>'u-color-19'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65336254080585722)
+,p_lov_disp_sequence=>24
+,p_lov_disp_value=>'Color 20'
+,p_lov_return_value=>'u-color-20'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65336653226585722)
+,p_lov_disp_sequence=>25
+,p_lov_disp_value=>'Color 21'
+,p_lov_return_value=>'u-color-21'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65337090121585721)
+,p_lov_disp_sequence=>26
+,p_lov_disp_value=>'Color 22'
+,p_lov_return_value=>'u-color-22'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65337419923585721)
+,p_lov_disp_sequence=>27
+,p_lov_disp_value=>'Color 23'
+,p_lov_return_value=>'u-color-23'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65337875059585721)
+,p_lov_disp_sequence=>28
+,p_lov_disp_value=>'Color 24'
+,p_lov_return_value=>'u-color-24'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65338290215585721)
+,p_lov_disp_sequence=>29
+,p_lov_disp_value=>'Color 25'
+,p_lov_return_value=>'u-color-25'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65338646744585721)
+,p_lov_disp_sequence=>30
+,p_lov_disp_value=>'Color 26'
+,p_lov_return_value=>'u-color-26'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65339011297585720)
+,p_lov_disp_sequence=>31
+,p_lov_disp_value=>'Color 27'
+,p_lov_return_value=>'u-color-27'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65339419795585720)
+,p_lov_disp_sequence=>32
+,p_lov_disp_value=>'Color 28'
+,p_lov_return_value=>'u-color-28'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65339853408585720)
+,p_lov_disp_sequence=>33
+,p_lov_disp_value=>'Color 29'
+,p_lov_return_value=>'u-color-29'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65340284362585720)
+,p_lov_disp_sequence=>34
+,p_lov_disp_value=>'Color 30'
+,p_lov_return_value=>'u-color-30'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65340626559585719)
+,p_lov_disp_sequence=>35
+,p_lov_disp_value=>'Color 31'
+,p_lov_return_value=>'u-color-31'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65341074392585719)
+,p_lov_disp_sequence=>36
+,p_lov_disp_value=>'Color 32'
+,p_lov_return_value=>'u-color-32'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65341485381585719)
+,p_lov_disp_sequence=>37
+,p_lov_disp_value=>'Color 33'
+,p_lov_return_value=>'u-color-33'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65341883755585719)
+,p_lov_disp_sequence=>38
+,p_lov_disp_value=>'Color 34'
+,p_lov_return_value=>'u-color-34'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65342250081585719)
+,p_lov_disp_sequence=>39
+,p_lov_disp_value=>'Color 35'
+,p_lov_return_value=>'u-color-35'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65342619462585718)
+,p_lov_disp_sequence=>40
+,p_lov_disp_value=>'Color 36'
+,p_lov_return_value=>'u-color-36'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65343092555585718)
+,p_lov_disp_sequence=>41
+,p_lov_disp_value=>'Color 37'
+,p_lov_return_value=>'u-color-37'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65343447417585718)
+,p_lov_disp_sequence=>42
+,p_lov_disp_value=>'Color 38'
+,p_lov_return_value=>'u-color-38'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65343839682585718)
+,p_lov_disp_sequence=>43
+,p_lov_disp_value=>'Color 39'
+,p_lov_return_value=>'u-color-39'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65344244847585717)
+,p_lov_disp_sequence=>44
+,p_lov_disp_value=>'Color 40'
+,p_lov_return_value=>'u-color-40'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65344613122585717)
+,p_lov_disp_sequence=>45
+,p_lov_disp_value=>'Color 41'
+,p_lov_return_value=>'u-color-41'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65345004075585717)
+,p_lov_disp_sequence=>46
+,p_lov_disp_value=>'Color 42'
+,p_lov_return_value=>'u-color-42'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65345491906585717)
+,p_lov_disp_sequence=>47
+,p_lov_disp_value=>'Color 43'
+,p_lov_return_value=>'u-color-43'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65345887907585716)
+,p_lov_disp_sequence=>48
+,p_lov_disp_value=>'Color 44'
+,p_lov_return_value=>'u-color-44'
+);
+wwv_flow_api.create_static_lov_data(
+ p_id=>wwv_flow_api.id(65346234033585716)
+,p_lov_disp_sequence=>49
+,p_lov_disp_value=>'Color 45'
+,p_lov_return_value=>'u-color-45'
+);
+wwv_flow_api.component_end;
+end;
+/
---
+++ apex/dynamic_action_plugin_de_vsbuedingen_colorful_inline_popup.sql
@@ -0,0 +1,130 @@
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- ORACLE Application Express (APEX) export file
+--
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_210200 or as the owner (parsing schema) of the application.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_api.import_begin (
+ p_version_yyyy_mm_dd=>'2021.10.15'
+,p_release=>'21.2.0'
+,p_default_workspace_id=>27635782693646703
+,p_default_application_id=>501
+,p_default_id_offset=>65309767924897831
+,p_default_owner=>'PROD'
+);
+end;
+/
+ 
+prompt APPLICATION 501 - Plugin  - Colorful Inline Popup
+--
+-- Application Export:
+--   Application:     501
+--   Name:            Plugin  - Colorful Inline Popup
+--   Date and Time:   11:29 Tuesday November 8, 2022
+--   Exported By:     JSTARK
+--   Flashback:       0
+--   Export Type:     Component Export
+--   Manifest
+--     PLUGIN: 65418063809743154
+--   Manifest End
+--   Version:         21.2.0
+--   Instance ID:     203728999270495
+--
+
+begin
+  -- replace components
+  wwv_flow_api.g_mode := 'REPLACE';
+end;
+/
+prompt --application/shared_components/plugins/dynamic_action/de_vsbuedingen_colorful_inline_popup
+begin
+wwv_flow_api.create_plugin(
+ p_id=>wwv_flow_api.id(65418063809743154)
+,p_plugin_type=>'DYNAMIC ACTION'
+,p_name=>'DE.VSBUEDINGEN.COLORFUL_INLINE_POPUP'
+,p_display_name=>'VS - Colorful Inline Popup'
+,p_category=>'EFFECT'
+,p_supported_ui_types=>'DESKTOP:JQM_SMARTPHONE'
+,p_plsql_code=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'function render ',
+'  ( p_dynamic_action in apex_plugin.t_dynamic_action',
+'  , p_plugin         in apex_plugin.t_plugin ',
+'  )',
+'return apex_plugin.t_dynamic_action_render_result',
+'as',
+'    l_result     apex_plugin.t_dynamic_action_render_result;',
+'',
+'begin',
+'    ',
+'    --debug',
+'    if apex_application.g_debug ',
+'    then',
+'        apex_plugin_util.debug_dynamic_action',
+'          ( p_plugin         => p_plugin',
+'          , p_dynamic_action => p_dynamic_action',
+'          );',
+'    end if;',
+'    ',
+'    l_result.javascript_function := ''function() {''',
+'        ||''const waitForLoading = function(selector, callback) {if (selector.length === 0) {callback();} else {setTimeout(function() {waitForLoading(selector, callback);}, 250);}};''',
+'        ||''''',
+'        ||''const closestHtml = $(this.triggeringElement).closest("html");''',
+'        ||''const pageId = closestHtml.closest(''''[class^="page"]'''').attr("class").split(/\s+/).filter(x => x.startsWith("page-"))?.[0].replace("page-", "");''',
+'        ||''const popupSelector = `#PopupLov_${pageId}_${this.triggeringElement.id}_dlg`;''',
+'        ||''''',
+'        ||''function addValueAsClass() {''',
+'        ||''    waitForLoading($(top.parent.document).find(popupSelector).find("span.u-Processing"), function() {''',
+'        ||''        setTimeout(function() {''',
+'        ||''            $(top.parent.document).find(popupSelector).find("li.a-IconList-item").each(function () {''',
+'        ||''                const _this = $(this);''',
+'        ||''                _this.addClass(_this.data("id"));''',
+'        ||''            });''',
+'        ||''        }, 150);''',
+'        ||''    });''',
+'        ||''}''',
+'        ||''addValueAsClass();''',
+'        ||''''',
+'        ||''$(top.parent.document).find(popupSelector).find(".a-GV-loadMoreButton").click(() => addValueAsClass());''',
+'    ||''}'';',
+'',
+'    return l_result;',
+'end render;'))
+,p_api_version=>2
+,p_render_function=>'render'
+,p_standard_attributes=>'ITEM'
+,p_substitute_attributes=>true
+,p_subscribe_plugin_settings=>true
+,p_help_text=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'This Plugin can be used to style a Popup LOVs Inline Popup.',
+'It uses the ListOfValues "Return Value" and set it as the list entry class.',
+'',
+'For Example:',
+'A Popup LOV to select a class from Universal Theme (https://apex.oracle.com/pls/apex/r/apex_pm/ut/color-and-status-modifiers).',
+'Color 1, 2 and 3 should be selectable.',
+'The ListOfValue for the item should be:',
+'Color 1 : u-color-1',
+'Color 2 : u-color-2',
+'Color 3 : u-color-3',
+'',
+'So each list item gets the corresponding class'))
+,p_version_identifier=>'0.1'
+,p_about_url=>'https://github.com/aerztliche-verrechnungsstelle-buedingen/apex-colorful-inline-popup'
+);
+end;
+/
+prompt --application/end_environment
+begin
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
